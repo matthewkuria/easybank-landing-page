3b5c960d8f27aebdf7880ecca291816bb3a8b2e2
matthewkuria <mathewkwachira@gmail.com>
2024-09-06 18:20:34 +0300

    Fix the image positioning

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import Button from "./Button"
 export default function Hero() {
     return (
         <section className="flex flex-col-reverse items-center md:flex-row max-w-full overflow-x-hidden mt-4">
-            <div className=" w-full md:w-1/2 flex flex-col text-left items-center   mt-10">
+            <div className=" w-full md:w-1/2 flex flex-col text-left items-center md:p-10  mt-10">
                 <h1 className="heading text-4xl">Next generation digital banking</h1>
                 <p className="">Take your financial life online. Your Easybank
                     account will be a one-stop-shop for spending, saving, budgeting,
@@ -11,8 +11,8 @@ export default function Hero() {
                 </p>
                 <Button />
             </div>
-            <div className="intro-image min-h-72">
-                <img src={imageMockUps} alt="background image mobile" height={500} width={500} className=" absolute -top-0 right-0 md:-right-28 z-0" />
+            <div className="intro-image min-h-72 md:h-full">
+                <img src={imageMockUps} alt="background image mobile" height={500} width={500} className=" absolute -top-20 md:-top-5 right-0 md:-right-28 z-0" />
             </div>            
         </section>
     )
