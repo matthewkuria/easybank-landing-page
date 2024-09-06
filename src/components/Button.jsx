0aa7c6ce4a7aefdbe712cea8cfbf8f403ea4ce51
matthewkuria <mathewkwachira@gmail.com>
2024-09-06 17:09:56 +0300

    React the bg-gradient color using the custom colors

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 export default function Button() {
     return (
-        <button className="btn hidden md:flex lg:block bg-gradient-to-r from-teal-400 to-blue-500 hover:from-teal-300 hover:to-blue-400 text-white font-bold rounded-full px-8 py-2">
+        <button className="btn hidden md:flex lg:block bg-gradient-to-r from-LimeGreen to-BrightCyan hover:opacity-70 text-LightGrayishBlue font-bold rounded-full px-8 py-2">
             Request Invite
         </button>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,26 @@ content: [
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+     screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      // Primary colors
+      'DarkBlue': 'hsl(233, 26%, 24%)',
+      'LimeGreen': 'hsl(136, 65%, 51%)',
+      'BrightCyan': 'hsl(192, 70%, 51%)',
+
+      // Neutral colors
+      
+     'GrayishBlue': 'hsl(233, 8%, 62%)',
+     'LightGrayishBlue': 'hsl(220, 16%, 96%)',
+     'VeryLightGray': 'hsl(0, 0%, 98%)',
+      'White':'hsl(0, 0%, 100%)',
+
+    },
     extend: {},
   },
   plugins: [],
