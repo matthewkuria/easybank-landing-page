1dd1ea44f8e87f677c6cd98335d6df4f624d2919
matthewkuria <mathewkwachira@gmail.com>
2024-09-06 16:56:08 +0300

    add public Sans fonts

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import NavBar from './components/NavBar'
+import Hero from './components/Hero'
 
 function App() {
 
   return (
     <main className="">
       <NavBar />
+      <Hero />
     </main>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+export default function Hero() {
+    return (
+        <section className="flex flex-col">
+            <div className="">
+
+            </div>
+            <div className="">
+
+            </div>            
+        </section>
+    )
+
+}
