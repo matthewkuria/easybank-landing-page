cf97c0c3ce668e63974da34da95d3101159e1411
matthewkuria <mathewkwachira@gmail.com>
2024-09-07 09:23:33 +0300

    Add padding 5 to the hero text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import Button from "./Button"
 export default function Hero() {
     return (
         <section className="flex flex-col-reverse items-center md:flex-row max-w-full overflow-x-hidden mt-4">
-            <div className=" w-full md:w-1/2 flex flex-col text-left items-center md:p-10  mt-10">
+            <div className=" w-full md:w-1/2 flex flex-col text-left items-center md:p-10  mt-10 p-5">
                 <h1 className="heading text-4xl">Next generation digital banking</h1>
                 <p className="">Take your financial life online. Your Easybank
                     account will be a one-stop-shop for spending, saving, budgeting,
