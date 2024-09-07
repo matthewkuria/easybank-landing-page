92af00487154fba5e7d05509f348ced4a62ea430
matthewkuria <mathewkwachira@gmail.com>
2024-09-07 09:25:40 +0300

    Add spacing to the paragraph in the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,9 +3,9 @@ import Button from "./Button"
 export default function Hero() {
     return (
         <section className="flex flex-col-reverse items-center md:flex-row max-w-full overflow-x-hidden mt-4">
-            <div className=" w-full md:w-1/2 flex flex-col text-left items-center md:p-10  mt-10 p-5">
+            <div className=" w-full md:w-1/2 flex flex-col text-left items-center md:p-20  mt-20 p-5">
                 <h1 className="heading text-4xl">Next generation digital banking</h1>
-                <p className="">Take your financial life online. Your Easybank
+                <p className="my-3">Take your financial life online. Your Easybank
                     account will be a one-stop-shop for spending, saving, budgeting,
                     investing, and much more.
                 </p>
