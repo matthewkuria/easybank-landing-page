039ceec96e54a22b1f260065b0c968df25805ec9
matthewkuria <mathewkwachira@gmail.com>
2024-09-07 09:47:54 +0300

    add backround image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,8 @@ import imageMockUps from "../assets/images/image-mockups.png"
 import Button from "./Button"
 export default function Hero() {
     return (
-        <section className="flex flex-col-reverse items-center md:flex-row max-w-full overflow-x-hidden mt-4">
-            <div className=" w-full md:w-1/2 flex flex-col text-left items-center md:p-20  mt-20 p-5">
+        <div className="container flex flex-col-reverse  md:flex md:flex-row items-center   mt-4">
+            <div className=" w-full md:w-1/2 flex flex-col text-left  md:p-20  mt-10 p-5">
                 <h1 className="heading text-4xl">Next generation digital banking</h1>
                 <p className="my-3">Take your financial life online. Your Easybank
                     account will be a one-stop-shop for spending, saving, budgeting,
@@ -11,10 +11,10 @@ export default function Hero() {
                 </p>
                 <Button />
             </div>
-            <div className="intro-image min-h-72 md:h-full">
+            <div className="intro-image md:w-1/2 min-h-72 md:h-full">
                 <img src={imageMockUps} alt="background image mobile" height={500} width={500} className=" absolute -top-20 md:-top-5 right-0 md:-right-28 z-0" />
             </div>            
-        </section>
+        </div>
     )
 
 }
