e8901bb2f0f5cd86e7032e62419fb9fa9d989163
matthewkuria <mathewkwachira@gmail.com>
2024-09-08 20:48:51 +0300

    Add background color

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import imageMockUps from "../assets/images/image-mockups.png"
 import Button from "./Button"
 export default function Hero() {
     return (
-        <div className="container flex flex-col-reverse  md:flex md:flex-row items-center   mt-4">
+        <div className="intro-image flex flex-col-reverse  md:flex md:flex-row items-center   mt-4">
             <div className=" w-full md:w-1/2 flex flex-col text-left  md:p-20  mt-10 p-5">
                 <h1 className="heading text-4xl">Next generation digital banking</h1>
                 <p className="my-3">Take your financial life online. Your Easybank
@@ -11,7 +11,7 @@ export default function Hero() {
                 </p>
                 <Button />
             </div>
-            <div className="intro-image md:w-1/2 min-h-72 md:h-full">
+            <div className=" md:w-1/2 min-h-72 md:h-full">
                 <img src={imageMockUps} alt="background image mobile" height={500} width={500} className=" absolute -top-20 md:-top-5 right-0 md:-right-28 z-0" />
             </div>            
         </div>
